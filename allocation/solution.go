1b562a86ba52acc9e47e94d14ed642ae7803a4bf
ivivanov <ivanivanov12@gmail.com>
2025-05-30 11:55:10 +0300

    feat(allocation): min heap POC

--- allocation/solution.go
+++ allocation/solution.go
@@ -1,1 +1,25 @@
 package allocation
+
+type DataCenter struct {
+	baseRisk   int
+	actualRisk int
+}
+
+type MinHeap []DataCenter
+
+// sort.Interface methods
+func (mh MinHeap) Len() int           { return len(mh) }
+func (mh MinHeap) Swap(i, j int)      { mh[i], mh[j] = mh[j], mh[i] }
+func (mh MinHeap) Less(i, j int) bool { return mh[i].actualRisk < mh[j].actualRisk }
+
+// heap.Interface methods
+func (mh *MinHeap) Push(x any) { *mh = append(*mh, x.(DataCenter)) }
+
+func (mh *MinHeap) Pop() any {
+	deref := *mh
+	l := len(deref)
+	last := deref[l-1]
+	*mh = deref[0 : l-1]
+
+	return last
+}
--- allocation/solution_test.go
+++ allocation/solution_test.go
@@ -1,1 +1,78 @@
 package allocation
+
+import (
+	"container/heap"
+	"testing"
+)
+
+func TestMinIntHeap(t *testing.T) {
+	testCases := []struct {
+		desc   string
+		risks  []int
+		expOut []int
+	}{
+		{
+			desc:   "Success",
+			risks:  []int{2, 77, 30, 20, 10},
+			expOut: []int{2, 10, 20, 30, 77},
+		},
+		{
+			desc:   "EmptyRisks_ShouldSucceed",
+			risks:  []int{},
+			expOut: []int{},
+		},
+		{
+			desc:   "NegativeRisks_ShouldSucceed",
+			risks:  []int{2, 77, 30, 20, -10},
+			expOut: []int{-10, 2, 20, 30, 77},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.desc, func(t *testing.T) {
+			mh := initMinHeap(t, tc.risks)
+			act := popAllNodes(t, mh)
+			assertEqualIntSlices(t, act, tc.expOut)
+		})
+	}
+}
+
+func assertEqualIntSlices(t *testing.T, act, exp []int) {
+	t.Helper()
+
+	actLen, expLen := len(act), len(exp)
+	if actLen != expLen {
+		t.Errorf("exp arr len: %v, act arr len: %v", expLen, actLen)
+	}
+
+	for i := 0; i < expLen; i++ {
+		if act[i] != exp[i] {
+			t.Errorf("exp: %v, act: %v", exp, act)
+		}
+	}
+}
+
+func initMinHeap(t *testing.T, risks []int) *MinHeap {
+	t.Helper()
+
+	mh := make(MinHeap, len(risks))
+	for i := 0; i < len(risks); i++ {
+		mh[i] = DataCenter{risks[i], risks[i]}
+	}
+	heap.Init(&mh)
+
+	return &mh
+}
+
+func popAllNodes(t *testing.T, mh *MinHeap) []int {
+	t.Helper()
+
+	result := make([]int, mh.Len())
+	i := 0
+	for mh.Len() > 0 {
+		result[i] = heap.Pop(mh).(DataCenter).actualRisk
+		i++
+	}
+
+	return result
+}
